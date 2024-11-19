3a4357642dde696368da8231ed82d9d1ccb50d17
Jayprakash <jayprakashyadab65@gmail.com>
2024-11-19 07:37:33 +0530

    operator

---
+++ Javascript/02_Operator/operator.js
@@ -0,0 +1,44 @@
+// operators 
+
+/* 
+Different Types of Operators
+
+1. Arithmetic Operators => + , /, -, **, %, 
+2. Assignment Operator
+3. Comparison Operator
+4. Logical Operator
+5. Unary Operator
+
+*/
+
+
+// 1. Arithmetic Operators
+
+let x = 20
+let y = 30
+
+console.log(x + y);
+console.log(x - y);
+console.log(x/y);
+console.log(x*y);
+console.log(x%y);
+console.log(x ** y);  // 20 to the power 30
+
+
+
+// 2. Assignment Operator
+
+let num = 1
+
+num = num + 1 
+console.log(num);
+
+// 3.Comparison Operator
+
+
+
+
+
+
+
+
