0f37a4d10f6c521ddb46aa218ef20dd92bc4425a
Jayprakash <jayprakashyadab65@gmail.com>
2024-11-24 21:40:20 +0530

    rivision

---
+++ Javascript/00_Revison/01_cond_Loops/index.js
@@ -0,0 +1,35 @@
+// if its rainy then dont go to collage 
+
+// Decison-maker: rain
+// Action: going to clg
+
+
+// const isItRaining = false; // bollean datatype
+
+// if(isItRaining){
+//     console.log('Dont go to clg');
+// }else{
+//     console.log('Go to clg');
+// }
+
+
+// Tracffic light based on the light color we stop, grt ready or go 
+
+// Decison-maker: light color
+// Action: control our vehicle
+
+// You coustomers/USERS wont listen to you. Error hadling.
+
+let lightColor = 'blue';
+
+if(lightColor === 'red'){
+    console.log('Stop');
+}else if(lightColor === 'yellow'){
+    console.log('get ready');
+}else if(lightColor === 'green'){
+    console.log('go');
+}else{
+    console.log("Invalid Output");
+    
+}
+
---
+++ Javascript/00_Revison/01_cond_Loops/loops.js
@@ -0,0 +1,35 @@
+// 1. for loop : when we want keep everything in same syantax.
+// 2. while loop: when i wnat to do the stuff manually and have dependence.
+// 3. do-while loop: when i wnat to do the stuff manually and have dependence. and even if conditin is not met initally i will run once.
+
+
+// for(let i=0; i<-5; i++){
+//     console.log(i)
+// }
+
+
+
+// I use for loop when i know the no. of iteration and while when i dont know the number of iterartion.
+
+// while() { }
+
+// let i=0;
+
+// while(i<=10){
+//     console.log(i);
+//     i++;
+    
+// }
+
+
+
+let numberOfNotification = 0;
+
+let i = 1;
+
+do{
+    console.log(`Look at mobile ${i}`);
+    i++;
+    
+}while (i <= numberOfNotification);
+
