bddb476eecbe9a83b4bd5acc0e81681f8d41d7c1
Jayprakash <jayprakashyadab65@gmail.com>
2024-11-24 21:39:39 +0530

    function

---
+++ Javascript/00_Revison/02_function/function.js
@@ -0,0 +1,26 @@
+// Function: increase reausability and costomize(value) as per the need.
+
+/*
+hello jay
+hello rahul
+hello avtar
+hello 
+
+*/
+
+
+function sayHello(name = 'User'){ 
+    console.log(`Hello ${name}`);
+    
+}
+
+sayHello("jay");
+sayHello("rahul")
+
+// parametrs: when aceppt the value
+// arugumnet: when we sen the value
+
+sayHello('')
+
+
+
